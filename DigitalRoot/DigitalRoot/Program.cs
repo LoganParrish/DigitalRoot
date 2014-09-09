8a1ffaddb8a98b302408212767f7427a6dd5e9eb
SeedPaths-User <lessdismissive@email.com>
2014-09-09 08:22:20 -0600

    DigitallyRooting

---
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalRoot("31337");
+            DigitalRoot("45734");
+            Console.ReadKey();
+        }
+        static void DigitalRoot(string rootThis)
+        {
+            int trueRoot = int.Parse(rootThis);
+            int answer = 0;
+            if(trueRoot > 0)
+            {
+                int total = trueRoot % 10;
+                trueRoot /= 10;
+                answer = answer + total;
+
+            }
+            else if (answer <= 9)
+            {
+                
+            }
+            Console.WriteLine(answer + 1);
+        }    
+    }
+}
